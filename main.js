3e444714a46d74f0902d25006aee692c6d56d178
Mandy Jacobsen <Mandy@Knightowltrivia.com>
2017-08-08 21:37:38 -0400

    add, delete, and create now working

--- main.js
+++ main.js
@@ -15,9 +15,70 @@ app.set('view engine', 'mst')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  database.any('SELECT * FROM "robots"').then(robots => {
+  database.any(`SELECT * FROM "robots"`).then(robots => {
     res.render('index', { robots: robots })
   })
+})
+
+app.get('/info/:id', (req, res) => {
+  const roboId = req.params.id
+  const getOneRobo = database.one(`SELECT * FROM "robots" WHERE id = $(id)`, { id: roboId }).then(getOneRobo => {
+    res.render('info', getOneRobo)
+  })
+})
+
+app.get('/info/edit/:id', (req, res) => {
+  const roboId = req.params.id
+  const getOneRobo = database.one(`SELECT * FROM "robots" WHERE id = $(id)`, { id: roboId }).then(getOneRobo => {
+    res.render('edit', getOneRobo)
+  })
+})
+
+app.get('/add', (req, res) => {
+  res.render('new')
+})
+
+app.post('/info/edit/:id', (req, res) => {
+  const roboId = req.params.id
+  database
+    .none(
+      `UPDATE "robots" SET name = $(name), city = $(city), country = $(country), job = $(job), company = $(company), email = $(email), phone = $(phone), university = $(university) WHERE id = $(id)`,
+      {
+        name: req.body.name,
+        city: req.body.city,
+        country: req.body.country,
+        job: req.body.job,
+        company: req.body.company,
+        email: req.body.email,
+        phone: req.body.phone,
+        university: req.body.university,
+        id: roboId
+      }
+    )
+    .then(res.redirect('/'))
+})
+
+app.post('/add', (req, res) => {
+  database
+    .none(
+      `INSERT INTO "robots" (name, city, country, job, company, email, phone, university) VALUES($(name), $(city), $(country), $(job), $(company), $(email), $(phone), $(university))`,
+      {
+        name: req.body.name,
+        city: req.body.city,
+        country: req.body.country,
+        job: req.body.job,
+        company: req.body.company,
+        email: req.body.email,
+        phone: req.body.phone,
+        university: req.body.university
+      }
+    )
+    .then(res.redirect('/'))
+})
+
+app.post('/info/delete/:id', (req, res) => {
+  const roboId = req.params.id
+  database.none(`DELETE FROM "robots" WHERE id = $(id)`, { id: roboId }).then(res.redirect('/'))
 })
 
 app.listen(3000, () => {
